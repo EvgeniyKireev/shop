01dadd5db29c055f650434d8ff679fc3852ffb48
Evgeny <zheka.mtb@gmail.com>
2020-03-25 23:09:59 +0300

    first commit

---
+++ myshop/orders/tasks.py
@@ -0,0 +1,17 @@
+from celery import task, shared_task
+from django.core.mail import send_mail
+from .models import Order
+
+@shared_task
+def order_created(order_id):
+    """
+    Задача для отправки уведомления по электронной почте при успешном создании заказа. 
+    """
+    order = Order.objects.get(id=order_id)
+    subject = 'Order nr. {}'.format(order_id)
+    message = f'Dear {order.first_name},\n\nYou have successfully placed an order. Your order id is {order.id}.'
+    mail_sent = send_mail(subject,
+                          message,
+                          'zheka.mtb@gmail.com',
+                          [order.email])
+    return mail_sent
